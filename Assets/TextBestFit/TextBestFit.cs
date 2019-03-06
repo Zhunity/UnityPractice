73343f53981cc9f29ded88c632392c7b598b437e
pawnietli <pawnietli@tencent.com>
2019-03-06 11:51:05 +0800

    文字自适应场景

---
+++ Assets/TextBestFit/TextBestFit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+[RequireComponent(typeof(ContentSizeFitter))]
+public class TextBestFit : MonoBehaviour {
+
+	public float MaxWidth = 450f;
+
+	private Text text;
+	private ContentSizeFitter contentSizeFitter;
+
+	void Start () {
+		text = GetComponent<Text>();
+		contentSizeFitter = GetComponent<ContentSizeFitter>();
+	}
+
+	private void Update()
+	{
+		if(text.preferredWidth <= MaxWidth)
+		{
+			text.resizeTextForBestFit = false;
+			contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+		}
+		else
+		{
+			text.resizeTextForBestFit = true;
+			contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
+		}
+		Debug.Log(text.preferredWidth);
+	}
+}
