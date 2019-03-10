f357623ee7195649b564f7c4cc98a6856a5e4668
Emilyli <poinseting@163.com>
2019-03-10 20:34:44 +0800

    List排序

---
+++ Assets/ListSort/ListSort.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListSort : MonoBehaviour
+{
+
+	public List<int> list;
+
+	private void OnGUI()
+	{
+		// 升序排列
+		if (GUI.Button(new Rect(0, 0, 100, 50), "AscendSort"))
+		{
+			list.Sort(AscendSort);
+			DumpList();
+		}
+
+		// 降序排列
+		if (GUI.Button(new Rect(200, 0, 100, 50), "DescendSort"))
+		{
+			list.Sort(DescendSort);
+			DumpList();
+		}
+	}
+
+	private void DumpList()
+	{
+		Debug.Log("-------------------------------------------------------------");
+		foreach (var item in list)
+		{
+			Debug.Log(item);
+		}
+		Debug.Log("-------------------------------------------------------------");
+	}
+
+	/// <summary>
+	/// 升序排列
+	/// </summary>
+	/// <param name="first"></param>
+	/// <param name="next"></param>
+	/// <returns></returns>
+	private int AscendSort(int first, int next)
+	{
+		return first.CompareTo(next);
+	}
+
+	/// <summary>
+	/// 降序排列
+	/// </summary>
+	/// <param name="first"></param>
+	/// <param name="next"></param>
+	/// <returns></returns>
+	private int DescendSort(int first, int next)
+	{
+		return next.CompareTo(first);
+	}
+}
