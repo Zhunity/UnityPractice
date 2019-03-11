cb040c0c298f85279ce3978ab86332c13a9a4e94
pawnietli <pawnietli@tencent.com>
2019-03-11 18:05:12 +0800

    UI透明度调整

---
+++ Assets/UIAlpha/Editor/UIAlphaInspector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+// 参考 https://www.xuanyusong.com/archives/3406
+
+[CustomEditor(typeof(UIAlpha))]
+public class UIAlphaInspector : Editor
+{
+	UIAlpha model;
+	public override void OnInspectorGUI()
+	{
+		model = target as UIAlpha;
+		float alpha = EditorGUILayout.FloatField("Alpha", model.Alpha);
+		if (model.Alpha != alpha)
+		{
+			model.Alpha = alpha;
+		}
+		base.DrawDefaultInspector();
+	}
+}
---
+++ Assets/UIAlpha/UIAlpha.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAlpha : MonoBehaviour {
+	public CanvasGroup Group;
+
+	private float _alpha;
+	public float Alpha
+	{
+		set
+		{
+			_alpha = value;
+			Group.alpha = value;
+			Debug.Log(Group.alpha);
+		}
+		get
+		{
+			return _alpha;
+		}
+	}
+}
