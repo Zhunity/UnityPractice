745d932398b517040f8f10e075c666b3c2f0bcd3
pawnietli <pawnietli@tencent.com>
2019-03-13 10:40:00 +0800

    GetProperties测试，只能找到get/set，不能找到函数、变量

--- Assets/AttributeReflection/AttributeReflection.cs
+++ Assets/AttributeReflection/AttributeReflection.cs
@@ -36,17 +36,50 @@ public class EventAttribute : Attribute
 
 public class Reflection : MonoBehaviour
 {
+	public bool yyyyyyyyyyyyyyyyyyyyyyyy
+	{
+		get;
+		set;
+	}
+	public PropertyInfo[] _bastAttr;
+	private PropertyInfo[] _thisAttr;
+
 	private void Awake()
 	{
-		PropertyInfo[] _attr = typeof(Reflection).GetProperties(BindingFlags.Public);
+		_bastAttr = typeof(Reflection).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static); // 只能找到get/set方法
+		_thisAttr = this.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+		PrintAttr();
+	}
+
+	private void PrintAttr()
+	{
+		Debug.LogError("-----------------------Reflection Attr Begin-----------------------");
+		int count = 0;
+		foreach (var item in _bastAttr)
+		{
+			Debug.Log(count++ + "\nName: " + item.Name + "\n PropertyType: " + item.PropertyType + "\n ReflectedType:" + item.ReflectedType
+				+ "\n DeclaringType:" + item.DeclaringType + "\n GetType:" + item.GetType() + "\n MemberType:" + item.MemberType + "\n ToString:" + item.ToString());
+		}
+		Debug.LogError("-----------------------Reflection Attr End-----------------------");
+
+		Debug.LogError("-----------------------GameObject Attr Begin-----------------------");
+		Debug.Log(this.GetType());
+		count = 0;
+		foreach (var item in _bastAttr)
+		{
+			Debug.Log(count++ + "\nName: " + item.Name + "\n PropertyType: " + item.PropertyType + "\n ReflectedType:" + item.ReflectedType
+				+ "\n DeclaringType:" + item.DeclaringType + "\n GetType:" + item.GetType() + "\n MemberType:" + item.MemberType + "\n ToString:" + item.ToString());
+		}
+		Debug.LogError("-----------------------GameObject Attr End-----------------------");
 	}
 }
 
 public class AttributeReflection : Reflection
 {
+	public float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
 
 	[EventAttribute("onClick", "Button")]
-	private void OnClick_Button()
+	public void OnClick_Button()
 	{
 		Debug.Log("Attribute Reflection");
 	}
