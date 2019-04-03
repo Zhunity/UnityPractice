081cea63c7e442cd7f60b47f6da9319509e4f79b
Emilyli <poinseting@163.com>
2019-04-03 20:50:19 +0800

    测试delegate

--- Assets/UIEventSeries/SMFrame/UI/UIView.cs
+++ Assets/UIEventSeries/SMFrame/UI/UIView.cs
@@ -74,19 +74,21 @@ namespace SMFrame
 		private void RegistUIEvent()
 		{
 			var thisMethodInfo = this.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-			foreach (var item in thisMethodInfo)
+			foreach (var eventMethod in thisMethodInfo)
 			{
-				if (item.ReflectedType != item.DeclaringType)
+				if (eventMethod.ReflectedType != eventMethod.DeclaringType)
 				{
 					continue;
 				}
-				var objList = item.GetCustomAttributes(typeof(UIEventAttribute), false);
+				var objList = eventMethod.GetCustomAttributes(typeof(UIEventAttribute), false);
 				foreach (var attr in objList)
 				{
 					if (attr is UIEventAttribute)
 					{
 						UIEventAttribute exe = attr as UIEventAttribute;
+						// 找到GameObjectName的控件，并在上面找到ComponentType脚本
 						var control = GetChild(exe.GameObjectName, exe.ComponentType);
+						// 在ComponentType脚本中找到事件名EventName
 						var memberInfo = control.GetType().GetMember(exe.EventName);
 						if(memberInfo == null)
 						{
@@ -99,25 +101,83 @@ namespace SMFrame
 						foreach (var member in memberInfo)
 						{
 							Debug.Log(i++ + "  "  + member.MemberType + "  " + member.Name);
+							// 判断EventName类型，比如Button的onClick就是MemberTypes.Property类型
 							if (member.MemberType == MemberTypes.Property)
 							{
 								PropertyInfo property = (PropertyInfo)member;
+								// 在EventName事件对象中找到AddListener方法
 								MethodInfo method = property.PropertyType.GetMethod("AddListener");
-								UnityAction target = () => { item.Invoke(this, null); };
+								// 将当前函数绑定到该控件的对应时间中
+								UnityAction target = () => { eventMethod.Invoke(this, null); };
 								method.Invoke(property.GetValue(control, null), new object[] { target });
 							}
-							else if(member.MemberType == MemberTypes.Field)
+							// Toggle的onValuedChange是Field类型
+							else if (member.MemberType == MemberTypes.Field)
 							{
 								FieldInfo field = (FieldInfo)member;
-								MethodInfo method = field.FieldType.GetMethod("AddListener");
-								UnityAction<bool> target = (value) => { item.Invoke(this, new object[] { value }); };
-								method.Invoke(field.GetValue(control), new object[] { target });
+								MethodInfo listenerMethod = field.FieldType.GetMethod("AddListener");
+								UnityAction<bool> target = (value) => { eventMethod.Invoke(this, new object[] { value }); };
+
+								ParameterInfo[] listenerParameter = listenerMethod.GetParameters(); // AddListener只有一个参数
+								Type paramType = listenerParameter[0].ParameterType;
+								object param = paramType.Assembly.CreateInstance(listenerParameter[0].Name); // 创建AddListen参数实例
+
+
+								ParameterInfo[] eventParameter = eventMethod.GetParameters();
+								object[] eventParams = new object[eventParameter.Length];
+								//var hh = delegate (eventParams)
+								//{ eventMethod.Invoke(this, eventParams); };
+
+								TestDelegate(delegate (int a)
+									{
+									});
+
+								DebugMethod(listenerMethod);
+
+
+								DebugMethod(eventMethod);
+
+
+
+								listenerMethod.Invoke(field.GetValue(control), new object[] { target });
 							}
 						}
 						
 					}
 				}
 			}
+		}
+
+		private void TestDelegate(Action<int> fun)
+		{
+			if(fun != null)
+			{
+				fun();
+			}
+		}
+
+		private void DebugMethod(MethodInfo method)
+		{
+#region 打印method
+			ParameterInfo[] parameter = method.GetParameters(); // 他妈的这里写成item了，难怪拿不到UnityEngine.Events.UnityAction`1[System.Boolean]
+			// 打印参数类型
+			int parameterInfoCount = 0;
+			foreach (ParameterInfo pi in parameter)
+			{
+				// 0   Parameter: Type=UnityEngine.Events.UnityAction`1[System.Boolean], Name=call
+				Debug.Log(parameterInfoCount++ + string.Format("   Parameter: Type={0}, Name={1}", pi.ParameterType, pi.Name));
+			}
+
+			// Name: AddListener
+			// ReflectedType:UnityEngine.Events.UnityEvent`1[System.Boolean]
+			// DeclaringType: UnityEngine.Events.UnityEvent`1[System.Boolean]
+			// GetType: System.Reflection.MonoGenericMethod
+			// MemberType:Method
+			// ToString:Void AddListener(UnityEngine.Events.UnityAction`1[System.Boolean])
+			// UnityEngine.Debug:Log(Object)
+			Debug.Log("\nName: " + method.Name + "\n ReflectedType:" + method.ReflectedType
++ "\n DeclaringType:" + method.DeclaringType + "\n GetType:" + method.GetType() + "\n MemberType:" + method.MemberType + "\n ToString:" + method.ToString());
+#endregion
 		}
 	}
 }
