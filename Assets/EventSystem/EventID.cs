2d4e1eb1b0b27cdb02c6e3b586ec971ee5223a5e
pawnietli <pawnietli@tencent.com>
2019-04-04 21:00:00 +0800

    定义几个事件id

--- Assets/EventSystem/EventID.cs
+++ Assets/EventSystem/EventID.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventID : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public enum EventID
+{
+	HelloWorld,
+	StartGame,
 }
