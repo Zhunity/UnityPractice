a2b97ffe76a5a36a04f4eb28383012865dffd378
lihua <poinseting@163.com>
2019-04-05 22:14:27 +0800

    测试不定参数委托

---
+++ Assets/DelegateParams/DelegateParams.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateParams : MonoBehaviour
+{
+	delegate void delegateParams(params object[] objs);
+
+	void Start()
+	{
+		delegateParams testNoParams = new delegateParams(ObjectParams);
+		if (null == testNoParams)
+		{
+			Debug.Log("testNoParams null");
+		}
+		else
+		{
+			Debug.Log("testNoParams not null");
+		}
+		testNoParams = ObjectParams;
+		testNoParams += ObjectParams;
+		testNoParams(1, "2");
+	}
+
+	private void NoParam1()
+	{
+		Debug.Log("No Param1");
+	}
+
+	private void NoParam2()
+	{
+		Debug.Log("No Param2");
+	}
+
+	private void OneParams(int num)
+	{
+		Debug.Log("One Params " + num);
+	}
+
+	private void DifferentTypeParams(int num, string str)
+	{
+		Debug.Log("Different Type Params " + num + "   " + str);
+	}
+
+	int test = 1;
+	private void ObjectParams(params object[] obj)
+	{
+		Debug.Log("Object Params " + test++);
+	}
+}
