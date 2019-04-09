b79be0e00cf8b39917fb96d0bc659cd5e6c587cf
pawnietli <pawnietli@tencent.com>
2019-04-09 16:29:29 +0800

    get/set在Inspector上显示

---
+++ Assets/FieldSerialize/FieldSerialize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldSerialize : MonoBehaviour {
+	/// <summary>
+	/// 只能显示不是默认实现的get/set，如果自己实现的get/set，可以参考UIAlpha
+	/// </summary>
+	[field:SerializeField]
+	public int Num
+	{
+		get;
+		set;
+	}
+}
