43e1f3d6fc741ef396ccfdadebbd317574c7df06
lihua <poinseting@163.com>
2019-04-22 15:47:24 +0800

    SQlite数据库测试

--- Assets/Sqlite/SqilteTest.cs
+++ Assets/Sqlite/SqilteTest.cs
@@ -6,14 +6,29 @@ public class SqilteTest : MonoBehaviour
 {
 	// https://www.jianshu.com/p/381ed9d59654
 
+	public SqliteDatabase Sqlite;
+
 	void Start()
     {
-        
-    }
+		//打开测试数据库
+		Sqlite = new SqliteDatabase("test.db");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		//添加数据
+		Sqlite.ExecuteNonQuery("insert into example (name) values('test name')");
+		PrintData();
+
+	}
+
+   private void PrintData()
+	{
+		DataTable dt = Sqlite.ExecuteQuery("SELECT * FROM example");
+		string name;
+		int id;
+		foreach(DataRow item in dt.Rows)
+		{
+			id = (int)item["id"];
+			name = (string)item["name"];
+			Debug.Log(id + "  " + name);
+		}
+	}
 }
