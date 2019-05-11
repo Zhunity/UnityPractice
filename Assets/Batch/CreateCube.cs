5c80ae7eb549dc9e809ea6f7b074f2afcd57a44c
lihua <poinseting@163.com>
2019-05-11 18:59:09 +0800

    用场景中GameObject测试动态合批（好像还不行）

--- Assets/Batch/CreateCube.cs
+++ Assets/Batch/CreateCube.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class CreateCube : MonoBehaviour
 {
-    // Start is called before the first frame update
+	public GameObject go;
     void Start()
     {
-		GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		for (int i = 0; i < 50; i++)
-
 		{
 			GameObject cube = GameObject.Instantiate(go);
-			cube.isStatic = true;
-			if(i % 10 == 0)
-			{
-				cube.transform.localScale = new Vector3(2, 2, 2);
-			}
-
 		}
 	}
 }
