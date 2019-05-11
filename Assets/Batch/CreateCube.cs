b8d9241d3058aae0ecbe85c91df054e9cb12b1de
lihua <poinseting@163.com>
2019-05-11 18:58:36 +0800

    用primitive作为模版测试动态合批（不行）

---
+++ Assets/Batch/CreateCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		for (int i = 0; i < 50; i++)
+
+		{
+			GameObject cube = GameObject.Instantiate(go);
+			cube.isStatic = true;
+			if(i % 10 == 0)
+			{
+				cube.transform.localScale = new Vector3(2, 2, 2);
+			}
+
+		}
+	}
+}
