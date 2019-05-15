6d604006df3b5301f07f294130fe91919a7af4bb
lihua <poinseting@163.com>
2019-05-15 21:20:04 +0800

    c#多重继承测试

---
+++ Assets/Inherit/Child.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// C# 不支持多重继承。但是，您可以使用接口来实现多重继承
+/// </summary>
+public class Child : ClassParent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Inherit/Parent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoParent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+		Debug.Log("MonoParent Awake");
+    }
+
+   
+}
+
+
+public class ClassParent
+{
+	public ClassParent()
+	{
+		Debug.Log("ClassParent Ctor");
+	}
+}
