4897ebafaa4545f819c00aa556661de1d8f540c0
lihua <poinseting@163.com>
2019-05-21 23:09:22 +0800

    打开unity动态合批选项

--- Assets/Batch/CreateCube.cs
+++ Assets/Batch/CreateCube.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// https://blog.csdn.net/wodownload2/article/details/81941384
+/// unity里面是可以设置是否动态合批处，设置的地方在：File->Build Settings->Player Settings 
+/// </summary>
 public class CreateCube : MonoBehaviour
 {
 	public GameObject go;
