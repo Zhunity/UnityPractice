49d1c61d7b7cc9a5adc02eba7e70bcf2db027946
lizh <poinseting@163.com>
2019-09-24 13:16:10 +0800

    Compute Shader运行成功

--- Assets/ComputeShader/Compute.cs
+++ Assets/ComputeShader/Compute.cs
@@ -31,5 +31,24 @@ public class Compute : MonoBehaviour
 		// Data 有3個float，一個 float 有 4 Byte，所以 3 * 4 = 12
 		ComputeBuffer inputBuffer = new ComputeBuffer(inputData.Length, 12);
 		ComputeBuffer outputBuffer = new ComputeBuffer(outputData.Length, 12);
+		int k = shader.FindKernel("CSMain");
+
+		// 写入GPU
+		inputBuffer.SetData(inputData);
+		shader.SetBuffer(k, "inputData", inputBuffer);
+
+		// 计算，并输出至CPU
+		shader.SetBuffer(k, "outputData", outputBuffer);
+		outputBuffer.GetData(outputData);
+
+		// 打印结果
+		for(int i = 0; i < outputData.Length; i ++)
+		{
+			Debug.Log(outputData[i].A + ", " + outputData[i].B + ", " + outputData[i].C);
+		}
+
+		// 释放
+		inputBuffer.Dispose();
+		outputBuffer.Dispose();
     }
 }
