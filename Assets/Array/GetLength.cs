5c183931d0620706d64f815e4d586246aa0616fb
lizh <poinseting@163.com>
2019-09-27 18:29:46 +0800

    二维数组的GetLength测试

---
+++ Assets/Array/GetLength.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetLength : MonoBehaviour
+{
+	public int row; // 行
+	public int col;	// 列
+	
+
+	private void Start()
+	{
+		//int[][] array = new int[row][1](); 这样会报错
+		int[,] array = new int[row, col];
+
+		Debug.LogFormat("GetLength(0):{0}\tLength:{1}", array.GetLength(0), array.Length);
+		// row:5 col:10 
+		// GetLength(0):5	Length:50
+		// GetLength(0) = row	Length = row * col
+	}
+}
