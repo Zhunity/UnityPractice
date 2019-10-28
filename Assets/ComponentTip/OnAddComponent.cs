4ed82c93f5184fbfef9ed4150c94e715b7580916
lizh <poinseting@163.com>
2019-10-28 17:26:34 +0800

    监听AddComponent事件

---
+++ Assets/ComponentTip/OnAddComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnAddComponent : UnityEditor.SceneManagement.AddedComponent
+{
+
+	public int a = 1;
+#if UNITY_EDITOR
+	public override void Apply(string prefabAssetPath)
+	{
+		Debug.Log("111111111111111 " + prefabAssetPath + "  " + instanceComponent);
+	}
+#endif
+}
