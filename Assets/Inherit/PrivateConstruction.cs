c775a1d9e2bfc2c5f2cea39810d348abb0f8074b
lihua <poinseting@163.com>
2019-12-13 21:31:21 +0800

    其他函数的访问属性的测试

--- Assets/Inherit/PrivateConstruction.cs
+++ Assets/Inherit/PrivateConstruction.cs
@@ -10,6 +10,21 @@ public class PrivateParent
 		Debug.Log("PrivateParent");
 	}
 
+	// 11、私有成员不可以用virtual
+	private void ParentPrivate()
+	{
+		Debug.Log("function ParentPrivate");
+	}
+
+	protected virtual void ParentProtected()
+	{
+		Debug.Log("ParentProtected");
+	}
+
+	public void ParentPublic()
+	{
+		Debug.Log("ParentPublic");
+	}
 }
 
 // 4、private PrivateParent()时，看起来不可以被继承，因为调用不了父类的构造函数
@@ -29,7 +44,28 @@ public class PublicChild : PrivateParent
 
 	protected PublicChild(string str)
 	{
-		Debug.Log("PublicChild call str");
+		Debug.Log("PublicChild get str");
+	}
+
+	// 7、这样是可以的
+	public void ParentPrivate()
+	{
+		Debug.Log("PublicChild call ParentPrivate");
+	}
+
+	// 8、public override void ParentProtected() 要在父类标记vitual，才可以用override，不标的话，目测可以同时存在
+	// 10、理所当然地在加了virtual后提示无法修改访问提示符
+	protected override void ParentProtected()
+	{
+		Debug.Log("PublicChild call ParentProtected");
+	}
+}
+
+public class Child2 : PrivateParent
+{
+	public void ParentPrivate()
+	{
+		Debug.Log("Child2 call ParentPrivate");
 	}
 }
 
@@ -38,6 +74,10 @@ public class PrivateConstruction : MonoBehaviour
     void Start()
     {
 		PublicChild child1 = new PublicChild();
+		Child2 child2 = new Child2();
+		// 9、理所当然地可以两个并存
+		child1.ParentPrivate();
+		child2.ParentPrivate();
 
 		//PublicChild child2 = new PublicChild(1);
 
