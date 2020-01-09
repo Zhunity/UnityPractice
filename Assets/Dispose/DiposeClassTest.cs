051187b3cc1e21983a08d69347e6e1bff7bad462
lihua <poinseting@163.com>
2020-01-09 08:19:42 +0800

    Dispose调试

---
+++ Assets/Dispose/DiposeClassTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisposableClass : IDisposable
+{
+	public int id;
+
+	public DisposableClass(int i)
+	{
+		id = i;
+		Debug.Log(id + " Disposable Create");
+	}
+
+	/// <summary>
+	/// 只有class才有析构函数
+	/// </summary>
+	~DisposableClass()
+	{
+		id = -1;
+		Debug.Log(id + "  Disposable Destroy");
+	}
+
+	public void Dispose()
+	{
+		id = -2;
+		Debug.Log(id + "  Disposable Dispose");
+	}
+}
+
+public class DiposeClassTest : MonoBehaviour
+{
+	int id = 1;
+	DisposableClass d;
+
+	void Start()
+	{
+		d = new DisposableClass(0);
+		Debug.Log(d.id + " before");
+		assignment();
+		Debug.Log(d.id + " after");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	private void assignment()
+	{
+		DisposableClass temp = d;
+		temp.id = id++;
+		temp.Dispose(); // **
+	}
+
+	/// <summary>
+	/// 还是要手动调用Dispose
+	/// </summary>
+	/// <param name="info"></param>
+	private void RefReturn(ref DisposableClass info)
+	{
+		//DisposableClass temp = info;
+		//temp.id = id++;
+		//info.Dispose();
+		info = new DisposableClass(id++);
+		//temp.Dispose();
+	}
+}
---
+++ Assets/Dispose/DiposeStructTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public struct DisposableStruct : IDisposable
+{
+	public int id;
+
+	public DisposableStruct(int i)
+	{
+		id = i;
+		Debug.Log(id + " Disposable Create");
+	}
+
+	/// <summary>
+	/// 只有class才有析构函数
+	/// </summary>
+	//~DisposableStruct()
+	//{
+	//	Debug.Log("Disposable Destroy");
+	//}
+
+	public void Dispose()
+	{
+		Debug.Log(id + "  Disposable Dispose");
+	}
+}
+
+public class DiposeStructTest : MonoBehaviour
+{
+	int id = 1;
+	DisposableStruct d;
+
+	void Start()
+    {
+		d = new DisposableStruct(0);
+		Debug.Log(d.id + " before");
+		RefReturn(ref d);
+		Debug.Log(d.id + " after");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	/// <summary>
+	/// struct类型的IDisposable，如果给另外一个赋值了，在另一个对象无用之后，需要手动Dispose
+	/// </summary>
+	private void assignment()
+	{
+		DisposableStruct temp = d;
+		temp.id = id ++;
+		temp.Dispose(); // **
+	}
+
+	private void RefReturn(ref DisposableStruct info)
+	{
+		DisposableStruct temp = info;
+		temp.id = id++;
+		info.Dispose();
+		info = new DisposableStruct(id++);
+		temp.Dispose();
+	}
+}
