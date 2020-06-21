feb6065305921592e257ad4e65b48a66dfb9f11b
lihua <poinseting@163.com>
2020-06-21 23:33:02 +0800

    测试Rect offset设置效果

---
+++ Assets/ScaleLayoutGroup/RectOffsetTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RectOffsetTest : MonoBehaviour
+{
+	public Vector2 offsetMax;
+	public Vector2 offsetMin;
+
+	RectTransform _rectTransform;
+	RectTransform rectTransform
+	{
+		get
+		{
+			if(_rectTransform == null)
+			{
+				_rectTransform = transform as RectTransform;
+			}
+			return _rectTransform;
+		}
+		set
+		{
+			_rectTransform = value;
+		}
+	}
+
+	private void Awake()
+	{
+		rectTransform = transform as RectTransform;
+	}
+
+	protected void OnValidate()
+	{
+		rectTransform.offsetMax = offsetMax;
+		rectTransform.offsetMin = offsetMin;
+	}
+}
