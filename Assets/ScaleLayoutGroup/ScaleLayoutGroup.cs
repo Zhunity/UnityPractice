81c9e5f8d5c14354b316ff3062c3c016c38e192b
lihua <poinseting@163.com>
2020-06-21 22:53:19 +0800

    当anchor为1 1 时成功了

--- Assets/ScaleLayoutGroup/ScaleLayoutGroup.cs
+++ Assets/ScaleLayoutGroup/ScaleLayoutGroup.cs
@@ -20,74 +20,95 @@ public class ScaleLayoutGroup : LayoutGroup
 		SetChildrenAlongAxis(1);
 	}
 
-	Dictionary<int, Vector2[]> childCorners = new Dictionary<int, Vector2[]>();
+	Dictionary<int, Vector2[]> childScales = new Dictionary<int, Vector2[]>();
 
 	protected override void Awake()
     {
 		base.Awake();
-		Vector2[] offsets = new Vector2[4];
-		Vector2[] anchors = new Vector2[4];
-		Vector3[] allCorners = new Vector3[4];
 		Vector2 size = rectTransform.rect.size;
-		Debug.Log(rectTransform.rect.min + "  " + rectTransform.rect.max + "  " + size + "  " + rectTransform.localPosition);
 
+		// 获取左下角坐标
 		Vector3[] thisCorners = new Vector3[4];
 		rectTransform.GetWorldCorners(thisCorners);
 		Vector3 thisMinPos = thisCorners[0];
+
+		Vector3[] childCorners = new Vector3[4];
 		for (int i = 0; i < rectTransform.childCount; i++)
 		{
 			RectTransform child = rectTransform.GetChild(i).transform as RectTransform;
-			child.GetWorldCorners(allCorners);
+			child.GetWorldCorners(childCorners);
 
-			var minCorner = allCorners[0];
-			var maxCorner = allCorners[3];
+			var minCorner = childCorners[0];
+			var maxCorner = childCorners[2];
 
 			Vector2[] scales = new Vector2[2];
 
+			scales[0] = GetCornerScale(size, thisMinPos, minCorner, child.anchorMin);
+			scales[1] = GetCornerScale(size, thisMinPos, maxCorner, child.anchorMax);
+			childScales.Add(child.GetHashCode(), scales);
+		}
 
-
-			for (int j = 0; j < 4; j++)
-			{
-				var anchorPos = Vector2.Scale(anchors[j], size);
-				var cornerPos = allCorners[j] - thisMinPos;
-				float x = (cornerPos.x - anchorPos.x) / size.x;
-				float y = (cornerPos.y - anchorPos.y) / size.y;
-				scales[i] = new Vector2(x, y);
-				Debug.Log("anchors[j] " + anchors[j] + "  childCorner:" + cornerPos + "  anchorPos: " + (anchorPos) + "  scales:" + scales[i]);
-			}
+		foreach (var item in childScales)
+		{
+			Debug.Log(item.Key + "  " + item.Value[0] + "  " + item.Value[1]);
 		}
 	}
 
-	public void GetCornerPercent(Vector2 parentSize, Vector3 parentMinCorner, Vector3 corner, Vector2 anchor)
+	public Vector2 GetCornerScale(Vector2 parentSize, Vector3 parentMinCorner, Vector3 corner, Vector2 anchor)
 	{
-
+		var anchorPos = Vector2.Scale(anchor, parentSize);
+		var cornerPos = corner - parentMinCorner;
+		float x = (cornerPos.x - anchorPos.x) / parentSize.x;
+		float y = (cornerPos.y - anchorPos.y) / parentSize.y;
+		Vector2 scale = new Vector2(x, y);
+		Debug.Log("cornerPos  " + cornerPos + " anchorPos: " + anchorPos + " parentSize:" + parentSize);
+		return scale;
 	}
 
-	public int offsetMaxX;
-	public int offsetMaxY;
-
-	protected override void OnValidate()
+	public void SetChildrenRect(Vector2 parentSize, RectTransform child, Vector2 scale, int axis, bool isMax)
 	{
-		base.OnValidate();
-		rectTransform.offsetMax = new Vector2(offsetMaxX, offsetMaxY);
-		
+		float scaleAxisValue = scale[axis];
+		Vector2 anchor = isMax ? child.anchorMax : child.anchorMin;
+		float anchorPos = anchor[axis] * parentSize[axis];
+
+		float cornerPos = scaleAxisValue * parentSize[axis] + anchorPos;
+
+		Vector2 offset;
+		if (isMax)
+		{
+			offset = child.offsetMax;
+			offset[axis] = cornerPos - parentSize[axis];
+			Debug.Log("axis  " + axis + " isMax " + isMax + "  parentSize " + parentSize + "  cornerPos :" + cornerPos + " offset " + offset);
+			child.offsetMax = offset;
+		}
+		else
+		{
+			offset = child.offsetMin;
+			offset[axis] = cornerPos;
+			Debug.Log("axis  " + axis + " isMax " + isMax + "  parentSize " + parentSize + "  cornerPos :" + cornerPos + " offset " + offset);
+			child.offsetMin = offset;
+		}
+	}
+
+	public void SetChildOffset(RectTransform child, int axis)
+	{
+
 	}
 
 	protected void SetChildrenAlongAxis(int axis)
 	{
-		float size = rectTransform.rect.size[axis];
-		Vector2[] offsets = new Vector2[4];
-		Vector2[] anchors = new Vector2[4];
+		Vector2 size = rectTransform.rect.size;
+
+		Vector2[] scales = new Vector2[2];
 		for (int i = 0; i < rectChildren.Count; i++)
 		{
 			RectTransform child = rectChildren[i];
-			GetOffset(child, offsets);
-			GetAnchors(child, anchors);
-			for(int j = 0; j < 4; j ++)
+			if(!childScales.TryGetValue(child.GetHashCode(), out scales))
 			{
-				float anchorPos = anchors[j][axis] * size;
-
+				continue;
 			}
+			SetChildrenRect(size, child, scales[0], axis, false);
+			SetChildrenRect(size, child, scales[1], axis, true);
 		}
 	}
 
