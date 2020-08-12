cecdbac39980f66f4c1d2463008267c60773991d
lihua <poinseting@163.com>
2020-08-12 22:57:08 +0800

    测试继承Start

---
+++ Assets/Override/Base.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("Base");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
