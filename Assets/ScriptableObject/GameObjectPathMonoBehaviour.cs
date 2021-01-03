4340175e2b84636036afe414903eb252815cf706
lihua <poinseting@163.com>
2021-01-03 23:50:08 +0800

    测试ScriptObject

---
+++ Assets/ScriptableObject/GameObjectPathMonoBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPathMonoBehaviour : MonoBehaviour
+{
+	public GameObjectPathSctiptObject sobj;
+
+	private void Awake()
+	{
+		Debug.Log(sobj, sobj.hi);
+	}
+}
---
+++ Assets/ScriptableObject/GameObjectPathSctiptObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameObjectPathSctiptObject : ScriptableObject
+{
+	public GameObject hi;
+
+
+	[MenuItem("Tools/CreateConfig")]
+	 private static void Create()
+	{
+		var sobj = ScriptableObject.CreateInstance<GameObjectPathSctiptObject>();
+		GameObject go = Selection.activeGameObject;
+		sobj.hi = go;
+		AssetDatabase.CreateAsset(sobj, "Assets/ScriptableObject/GameObjectPath.asset");
+	} 
+
+}
